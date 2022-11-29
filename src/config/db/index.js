835482403317ec709a42884a45ea538998b27f2c
LeHoangAnh21 <84314151+LeHoangAnh21@users.noreply.github.com>
2022-11-29 17:02:16 +0700

    ok

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose')
 async function connect() {
 
 	try {
-		await mongoose.connect('mongodb://localhost:27017/LittlePeaData', {
+		await mongoose.connect('mongodb+srv://arsper11:lehoanganh21@cluster0.bipvm.mongodb.net/LittlePeaDatabases', {
 			useNewUrlParser: true,
 			useUnifiedTopology: true
 		});
 		console.log('Database connect successfully')
 	}
-
+	
 	// try {
-	// 	await mongoose.connect('mongodb+srv://arsper11:lehoanganh21@cluster0.bipvm.mongodb.net/LittlePeaDatabases', {
+	// 	await mongoose.connect('mongodb://localhost:27017/LittlePeaData', {
 	// 		useNewUrlParser: true,
 	// 		useUnifiedTopology: true
 	// 	});
